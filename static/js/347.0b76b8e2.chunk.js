2c94f6f867996407efefee64a52f767da11453f7
mohmyyy <iammohmy@gmail.com>
2023-04-10 03:35:06 +0530

    Updates

---
+++ static/js/347.0b76b8e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkecom_website=self.webpackChunkecom_website||[]).push([[347],{1347:function(e,n,c){c.r(n);c(9212),c(7744);var s=c(184);n.default=function(){return(0,s.jsx)(s.Fragment,{children:(0,s.jsxs)("center",{className:"m-5",children:[(0,s.jsx)("h1",{children:"Could Not Find page"}),(0,s.jsx)("p",{children:"Try again after a decade"})]})})}}}]);
+//# sourceMappingURL=347.0b76b8e2.chunk.js.map
